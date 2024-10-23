4f3b82848505f166ae23e6610be7fb47fcfd9f8f
Sofie Larsen <Sofieamalierl@hotmail.com>
2024-10-23 11:43:26 +0200

    Icons added for home, menu, recipt and settings
    Missing back button and styling

--- app/(tabs)/_layout.tsx
+++ app/(tabs)/_layout.tsx
@@ -1,10 +1,42 @@
+import Ionicons from "@expo/vector-icons/Ionicons";
 import { Tabs } from "expo-router";
 
 export default function TabsLayout() {
-  return (
-    <Tabs>
-      <Tabs.Screen name="home" options={{ title: "Home" }} />
-      <Tabs.Screen name="settings" options={{ title: "Settings" }} />
-    </Tabs>
-  );
+    return (
+        <Tabs
+            screenOptions={{
+                headerShown: false,
+                tabBarActiveTintColor: "#ffd33d",
+            }}
+        >
+            <Tabs.Screen
+                name="home"
+                options={{
+                    title: "Home",
+                    tabBarIcon: ({ color, focused }) => <Ionicons name={focused ? "home-sharp" : "home-outline"} color={color} size={24} />,
+                }}
+            />
+            <Tabs.Screen
+                name="menu"
+                options={{
+                    title: "Menu",
+                    tabBarIcon: ({ color, focused }) => <Ionicons name={focused ? "menu-sharp" : "menu-outline"} color={color} size={24} />,
+                }}
+            />
+            <Tabs.Screen
+                name="receipt"
+                options={{
+                    title: "receipt",
+                    tabBarIcon: ({ color, focused }) => <Ionicons name={focused ? "receipt-sharp" : "receipt-outline"} color={color} size={24} />,
+                }}
+            />
+            <Tabs.Screen
+                name="settings"
+                options={{
+                    title: "Settings",
+                    tabBarIcon: ({ color, focused }) => <Ionicons name={focused ? "settings-sharp" : "settings-outline"} color={color} size={24} />,
+                }}
+            />
+        </Tabs>
+    );
 }
---
+++ app/(tabs)/menu.tsx
@@ -0,0 +1,9 @@
+import { View, Text } from "react-native";
+
+export default function Menu() {
+  return (
+    <View className="flex-1 justify-center items-center">
+      <Text>Menu page</Text>
+    </View>
+  );
+}
