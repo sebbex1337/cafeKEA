56b16aab8a0d231f7417f61af8397753de6fe688
sebbex1337 <sefort@hotmail.dk>
2024-10-23 11:17:19 +0200

    tabs work on my app

---
+++ app/(tabs)/settings.tsx
@@ -0,0 +1,9 @@
+import { View, Text } from "react-native";
+
+export default function Settings() {
+  return (
+    <View className="flex-1 justify-center items-center">
+      <Text>Settings page</Text>
+    </View>
+  );
+}
