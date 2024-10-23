2c596f1df0d2cca65b8eac94259392a6b1b62347
sebbex1337 <sefort@hotmail.dk>
2024-10-23 11:24:12 +0200

    added router to go back and fourth between tabs and index page

---
+++ app/index.tsx
@@ -0,0 +1,14 @@
+import { Link, useRouter } from "expo-router";
+import { View, Text, Pressable } from "react-native";
+
+export default function Index() {
+  const router = useRouter();
+  return (
+    <View>
+      <Text>Index page</Text>
+      <Pressable onPress={() => router.push(`/(tabs)/home`)}>
+        <Text>Go to home tabs</Text>
+      </Pressable>
+    </View>
+  );
+}
