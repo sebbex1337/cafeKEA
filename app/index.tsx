4436ec825bb4479137bd8576b664aa6d77c7ba5a
sebbex1337 <sefort@hotmail.dk>
2024-10-29 12:41:18 +0100

    added signup

--- app/index.tsx
+++ app/index.tsx
@@ -1,6 +1,6 @@
 import { useRouter } from "expo-router";
-import { View, Text, Pressable, TextInput } from "react-native";
-import { getAuth, onAuthStateChanged, signInWithEmailAndPassword } from "firebase/auth";
+import { View, Text, Pressable, TextInput, Alert } from "react-native";
+import { createUserWithEmailAndPassword, getAuth, onAuthStateChanged, signInWithEmailAndPassword } from "firebase/auth";
 import { app } from "../firebase";
 import { useEffect, useState } from "react";
 
@@ -8,8 +8,9 @@ const auth = getAuth(app);
 
 export default function Index() {
   const router = useRouter();
-  const [email, setEmail] = useState("");
-  const [password, setPassword] = useState("");
+  const [formDataLogin, setFormDataLogin] = useState({ email: "", password: "" });
+  const [formDataSignUp, setFormDataSignUp] = useState({ email: "", password: "", password2: "" });
+  const [signingUp, setSigningUp] = useState(false);
 
   useEffect(() => {
     const _auth = getAuth();
@@ -22,8 +23,25 @@ export default function Index() {
 
   async function login() {
     try {
-      const userCredential = await signInWithEmailAndPassword(auth, email, password);
+      const userCredential = await signInWithEmailAndPassword(auth, formDataLogin.email, formDataLogin.password);
       console.log("logged in " + userCredential.user.uid);
+    } catch (error) {
+      console.log(error);
+    }
+  }
+
+  async function signup() {
+    try {
+      if (formDataSignUp.password === formDataSignUp.password2) {
+        const userCredential = await createUserWithEmailAndPassword(
+          auth,
+          formDataSignUp.email,
+          formDataSignUp.password
+        );
+        console.log("signed up " + userCredential.user.uid);
+      } else {
+        Alert.alert("Passwords do not match");
+      }
     } catch (error) {
       console.log(error);
     }
@@ -31,18 +49,61 @@ export default function Index() {
 
   return (
     <View className="flex-1 justify-center items-center">
-      <Text>Login page</Text>
-      <TextInput placeholder="Email" className="w-[300] p-2" value={email} onChangeText={(email) => setEmail(email)} />
-      <TextInput
-        placeholder="Password"
-        secureTextEntry
-        className="w-[300] p-2"
-        value={password}
-        onChangeText={(password) => setPassword(password)}
-      />
-      <Pressable onPress={login} className="rounded-xl px-4 py-2 bg-blue-600 mb-4">
-        <Text className="text-white">Login</Text>
-      </Pressable>
+      {!signingUp && (
+        <>
+          <Text className="text-4xl font-bold pb-4">Login to cafeKEA</Text>
+          <TextInput
+            placeholder="Email"
+            className="w-[300] p-2"
+            value={formDataLogin.email}
+            onChangeText={(email) => setFormDataLogin({ ...formDataLogin, email })}
+          />
+          <TextInput
+            placeholder="Password"
+            secureTextEntry
+            className="w-[300] p-2"
+            value={formDataLogin.password}
+            onChangeText={(password) => setFormDataLogin({ ...formDataLogin, password })}
+          />
+          <Pressable onPress={login} className="rounded-xl px-4 py-2 bg-blue-600 mb-4">
+            <Text className="text-white">Login</Text>
+          </Pressable>
+          <Pressable onPress={() => setSigningUp(!signingUp)} className="rounded-xl px-4 py-2 bg-blue-600 mb-4">
+            <Text className="text-white">Sign up</Text>
+          </Pressable>
+        </>
+      )}
+      {signingUp && (
+        <>
+          <Text className="text-4xl font-bold pb-4">Register to cafeKEA</Text>
+          <TextInput
+            placeholder="Email"
+            className="w-[300] p-2"
+            value={formDataSignUp.email}
+            onChangeText={(email) => setFormDataSignUp({ ...formDataSignUp, email })}
+          />
+          <TextInput
+            placeholder="Password"
+            secureTextEntry
+            className="w-[300] p-2"
+            value={formDataSignUp.password}
+            onChangeText={(password) => setFormDataSignUp({ ...formDataSignUp, password })}
+          />
+          <TextInput
+            placeholder="Enter password again"
+            secureTextEntry
+            className="w-[300] p-2 pb-4"
+            value={formDataSignUp.password2}
+            onChangeText={(password2) => setFormDataSignUp({ ...formDataSignUp, password2 })}
+          />
+          <Pressable onPress={signup} className="rounded-xl px-4 py-2 bg-blue-600 mb-4">
+            <Text className="text-white">Create account</Text>
+          </Pressable>
+          <Pressable onPress={() => setSigningUp(!signingUp)} className="rounded-xl px-4 py-2 bg-blue-600 mb-4">
+            <Text className="text-white">Go to login</Text>
+          </Pressable>
+        </>
+      )}
       <Pressable onPress={() => router.replace(`/(tabs)/home`)}>
         <Text>Go to home tabs</Text>
       </Pressable>
