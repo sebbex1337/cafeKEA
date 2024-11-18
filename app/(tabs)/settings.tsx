4a1306265ba6f306613da733f292150d26f6c0fe
Sofie Larsen <Sofieamalierl@hotmail.com>
2024-11-18 11:06:17 +0100

    tank op virker og opdatere både home saldo og settings saldo

--- app/(tabs)/settings.tsx
+++ app/(tabs)/settings.tsx
@@ -1,14 +1,17 @@
 import { View, Text, Pressable, Modal, TextInput, Button } from "react-native";
 import { signOut } from "firebase/auth";
-import { auth } from "../../firebase";
+import { auth, database } from "../../firebase";
 import { useRouter } from "expo-router";
 import Saldo from "@/components/Saldo";
-import { useState } from "react";
+import { useEffect, useState } from "react";
+import { doc, getDoc, updateDoc } from "firebase/firestore";
 
 export default function Settings() {
+    const user = auth.currentUser;
     const router = useRouter();
     const [modalVisible, setModalVisible] = useState(false);
     const [amount, setAmount] = useState("");
+    const [saldo, setSaldo] = useState(0);
 
     async function sign_out() {
         try {
@@ -19,14 +22,52 @@ export default function Settings() {
         }
     }
 
-    function handleTankOp() {
+    useEffect(() => {
+        async function getUserSaldo() {
+            try {
+                if (user) {
+                    const docRef = doc(database, "users", user.uid);
+                    const docSnap = await getDoc(docRef);
+                    if (docSnap.exists()) {
+                        setSaldo(docSnap.data().saldo);
+                    } else {
+                        console.log("No such document!");
+                    }
+                }
+            } catch (error) {
+                console.log("Error getting saldo", error);
+            }
+        }
+        getUserSaldo();
+    }, [user]);
+
+    async function handleTankOp() {
         console.log("din nye saldo");
+        const enteredAmount = Number(amount);
+        if (!isNaN(enteredAmount) && user) {
+            const docRef = doc(database, "users", user.uid);
+            try {
+                const docSnap = await getDoc(docRef);
+                if (docSnap.exists()) {
+                    const currentSaldo = docSnap.data().saldo || 0;
+                    const newSaldo = currentSaldo + enteredAmount;
+                    await updateDoc(docRef, {
+                        saldo: newSaldo,
+                    });
+                    setSaldo(newSaldo);
+                }
+            } catch (error) {
+                console.log("Error getting saldo", error);
+            }
+        } else {
+            console.log("invalid input");
+        }
     }
 
     return (
         <View className="h-full bg-yellow-200">
             <View className="bg-red-300 items-center py-8">
-                <Saldo />
+                <Saldo userSaldo={saldo} />
                 <Pressable onPress={() => setModalVisible(true)} className="bg-[#ffd33d] py-2 px-4 rounded-xl mt-4">
                     <Text>Tank op</Text>
                 </Pressable>
